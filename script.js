9364dcf78dff93c162dd1919b6934cec08acbfb9
siafromspace <sharonyak15@gmail.com>
2022-05-24 19:05:12 +0100

    added filter functionality

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const list = document.querySelectorAll('.list');
+const itemBox = document.querySelectorAll('.itemBox')
 
 for (let i = 0; i < list.length; i++) {
     list[i].addEventListener("click", function () {
         removeActive()
         this.classList.add('active')
+
+        let dataFilter = this.getAttribute('data-filter')
+
+        for (let k = 0; k < itemBox.length; k++) {
+            itemBox[k].classList.remove('active')
+            itemBox[k].classList.add('hide')
+
+            if (itemBox[k].getAttribute('data-item') == dataFilter || dataFilter == 'all') {
+                itemBox[k].classList.remove('hide')
+                itemBox[k].classList.add('active')
+            }
+        }
     })
 }
 
